3d1873bd02eba495daac907035ecd999ec4ad350
pdwarkanath <dwarkanath.prabhu@gmail.com>
2020-07-16 03:05:23 -0500

    css

--- index.js
+++ index.js
@@ -7,6 +7,13 @@
 const express = require("express");
 const path = require("path");
 
+
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
 /**
  * App Variables
  */
@@ -15,11 +22,34 @@ const app = express();
 const port = process.env.PORT || "8000";
 
 /**
+ * Session Configuration
+ */
+
+const session = {
+  secret: "LoxodontaElephasMammuthusPalaeoloxodonPrimelephas",
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration
+ */
+
+/**
  *  App Configuration
  */
 
+
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Routes Definitions
@@ -27,6 +57,10 @@ app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
+});
+
+app.get("/user", (req, res) => {
+    res.render("user", {title: "Profile", userProfile: {nickname: "DK"}});
 });
 
 /**
